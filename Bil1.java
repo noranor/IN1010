65f739c10f183aaf2aa15819b7424642c36e47fd
noranor <33264001+noranor@users.noreply.github.com>
2019-01-14 20:28:49 +0100

    biler

---
+++ Bil1.java
@@ -0,0 +1,13 @@
+// Oppgave 1, Nora Skjelstad, IN1010, Oblig 0
+
+public class Bil1{
+	private int bilnummer;
+
+	public void settNummer(String bilnummer){
+		bilnummer = bilnummer;
+	}
+
+	public static void skrivUt(){
+		System.out.println("Jeg er en Bil");
+	}
+}
