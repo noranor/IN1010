625421d3148688d15f2225f623b136d43796a1fe
noranor <33264001+noranor@users.noreply.github.com>
2019-01-14 20:24:22 +0100

    first
    Need to fix my pc

---
+++ BilBruk1.java
@@ -0,0 +1,9 @@
+// Oppgave 1, Nora Skjelstad, IN1010, Oblig 0
+
+public class BilBruk1{
+	public static void main(String[] args) {
+		Bil1 minBil = new Bil1();
+
+		minBil.skrivUt();
+	}
+}
---
+++ BilBruk2.java
@@ -0,0 +1,7 @@
+public class BilBruk1{
+	public static void main(String[] args) {
+		Bil2 MinBil2 = new Bil2();
+		MinBil2.settNummer("DK94883");
+		Minbil1.skrivNummer("DK94883");
+	}
+}
